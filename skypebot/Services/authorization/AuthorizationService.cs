91a026f9ab5aab61d90f768fb21870fb8de99f14
Otto Remse <otto.remse@gmail.com>
2015-09-10 17:53:27 +0200

    Added skeleton for authorization service

---
+++ skypebot/Services/authorization/AuthorizationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skypebot.Services.authorization
+{
+    public class AuthorizationService : IChatBotService
+    {
+        public int Priority
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool CanHandleCommand(string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleCommand(string fromHandle, string fromDisplayName, string command, string parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
